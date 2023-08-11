f5e1b3d8ce00cfb78223a77bc7f21f74aeabbb51
ramon-braga <ramonvbraga@gmail.com>
2023-08-10 22:46:37 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const todoList = [];
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate
+        name,
+        dueDate
+    });
+
+    console.log(todoList);
+
+    inputElement.value = '';
+
+    renderTodoList();
+}
+
+function renderTodoList() {
+    let todoListHTML = '';
+
+    for (i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        const { name, dueDate } = todoObject;
+
+        todoListHTML += `
+        <div class="name-task">${name}</div>
+        <div class="due-date-task">${dueDate}</div>
+        <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        " class="delete-todo-button">Delete</button>
+        `;
+    }
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
